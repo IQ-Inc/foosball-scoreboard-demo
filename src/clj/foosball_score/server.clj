e31d4c826cf7ccc31664e460a1c7c523740f31ad
Ian McIntyre <ianpmcintyre@gmail.com>
2017-08-27 19:17:27 -0400

    Create events module

--- src/clj/foosball_score/server.clj
+++ src/clj/foosball_score/server.clj
@@ -4,27 +4,10 @@
   (:require 
     [foosball-score.handler :refer [app push-event!]]
     [foosball-score.serial :as serial]
+    [foosball-score.events :as events]
     [config.core :refer [env]]
-    [clojure.core.async :as async :refer [<! chan go-loop]]
     [org.httpkit.server :refer [run-server]])
   (:gen-class :main true))
-
-(def event-lookup
-  { "BD" :drop        ; black drop - not specified as a unique event
-    "GD" :drop        ; gold drop - not specified as a unique event
-    "BG" :black       ; black goal
-    "GG" :gold })     ; gold goal
-
-(def ^:private serial-event-chan (chan))
-
-(defn- serial-message-handler
-  "Get event from the serial module, and push it onto the
-  websocket channel. Runs forever. Invocation does not block"
-  [msg-chan]
-  (go-loop [msg (<! msg-chan)]
-    (if-let [event (get event-lookup msg)]
-      (push-event! event))
-      (recur (<! msg-chan))))
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (env :port) "3000"))
@@ -32,6 +15,5 @@
     (if-let [baud (read-string (nth args 1))]
       (serial/listen-on-port ser baud)
       (serial/listen-on-port ser))
-    (serial/add-serial-subscriber serial-event-chan)
-    (serial-message-handler serial-event-chan)
+    (serial/add-serial-subscriber (events/make-event-handler! push-event!))
     (run-server app {:port port :join? false})))
