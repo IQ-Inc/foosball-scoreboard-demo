e50e9804c5030fdee1c09f6a16ec269e7ce41daa
Ian McIntyre <ianpmcintyre@gmail.com>
2017-09-06 20:46:51 -0400

    Add avatar randomization
    Fix some spacing for the scorelist

--- src/cljs/foosball_score/players.cljs
+++ src/cljs/foosball_score/players.cljs
@@ -4,17 +4,53 @@
     [reagent.core :as reagent :refer [atom]]
     [foosball-score.util :refer [teams colors]]))
 
-(defonce players (atom []))
+;;;;;;;;;;
+;; Avatars
+;;;;;;;;;;
 
+;; Avatar icon names will follow the convention
+;;    avatar-face-icon-#.png
+;; where # is a number starting at 1 and a sequential increment from
+;; the previous avatar icon name. When you add a new avatar, increment
+;; number-of-avatars appropriately.
+(def all-avatar-imgs
+  (let [number-of-avatars 4]  ;; Change me if there are more avatars
+    (take number-of-avatars
+      (map #(str "/img/avatar-face-icon-" % ".png")
+        (iterate inc 1)))))
+
+(defonce avatars
+  (atom (shuffle all-avatar-imgs)))
+
+(defn- reset-avatars!
+  "Reset the available avatars randomly, and store them in the atom."
+  []
+  (reset! avatars (shuffle all-avatar-imgs)))
+
+(defn- pop-random-avatar!
+  "Returns the next available avatar, or nil if they're all gone"
+  []
+  (let [avatar (first @avatars)]
+    (swap! avatars rest)
+    avatar))
+
+;;;;;;;;;;;;;;;
+;; Players n'at
+;;;;;;;;;;;;;;;
+
+(defonce players (atom []))
 (defonce max-players 4)
 
 (defn- append-player
+  "Append player to players if there are fewer than max-players"
   [players player]
     (if (< (count players) max-players)
       (conj players player)
       players))
 
 (defn- next-team
+  "Get the team that will receive the next player.
+  Players are first assigned to black, then to gold, then black, then gold."
   []
   (if (even? (count @players))
     :black
@@ -23,13 +59,17 @@
 (defn add-player!
   "Add a player"
   [player]
-  (let [team (next-team)]
-    (swap! players append-player {:player player :team team})))
+  (let [team    (next-team)
+        avatar  (pop-random-avatar!)]
+    (swap! players append-player {:player player :team team
+                                  :avatar avatar})))
 
 (defn reset-players!
   "Reset the player list"
   []
-  (reset! players []))
+  (do
+    (reset! players [])
+    (reset-avatars!)))
 
 (defn- player-color
   [{:keys [team]}]
@@ -44,5 +84,5 @@
       (for [player sorted-players] ^{:key player}
         [:div.player 
           [:div {:style {:color (player-color player) }} (player :player)]
-          [:img.avatar {:src "/img/avatar-face-icon.png"}]
+          [:img.avatar {:src (player :avatar)}]
         ]))])
