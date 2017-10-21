718c87c29f2baf177bbbdf2a57d092a3dcf98866
Ian McIntyre <ianpmcintyre@gmail.com>
2017-10-21 18:21:55 -0400

    Update scores to use state

--- src/cljs/foosball_score/game.cljs
+++ src/cljs/foosball_score/game.cljs
@@ -8,45 +8,22 @@
     [foosball-score.util :refer [teams colors]]
     [foosball-score.state :refer [state]]))
 
-;; --------------------------------
-;; Atoms and constants
-
-(defonce scores
-  (atom (zipmap teams (cycle [0]))))
-
 (defonce score-times
   (atom '[]))
-
-(defonce max-score 5)
 
 ;; --------------------------------
 ;; Functions
 (defn new-game
   "Start a new game"
   []
-  (reset! scores
-    (zipmap teams (cycle [0])))
   (reset! score-times []))
-
-(defn game-over?
-  "Returns true if the game is over, else false"
-  []
-  (let [ss @scores]
-    (boolean (some #(>= % max-score) (vals ss)))))
-
-(defn point-for
-  "Increment a point for one of the teams"
-  [team time]
-  (if (and (some #(= team %) teams) (not (game-over?)))  
-    (do 
-      (swap! scores update-in [team] inc)
-      (swap! score-times conj {:team team :time time}))))
 
 (defn- scorecard-class
   "Change the scorecards class"
   [score]
-  (if (>= score max-score)
+  (if (>= score (:max-score @state))
     "blink"))
+
 ;; --------------------------------
 ;; Components
 
@@ -63,7 +40,7 @@
 (defn scoreboard-content
   "A team's scoreboard content"
   [team align]
-  (let [ss @scores
+  (let [ss (:scores @state)
         color (team colors)]
     [:div.scorecard {:class (scorecard-class (team ss))}
       [:h6 {:style {:color color :text-align align}} (string/upper-case (name team))]
