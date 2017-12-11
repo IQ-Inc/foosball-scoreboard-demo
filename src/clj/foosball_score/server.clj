99fa61216c336c9b021259cfe8f51f9d472bfb64
Ian McIntyre <ianpmcintyre@gmail.com>
2017-12-11 18:40:53 -0500

    Fix stripping of persistence artifacts
    Design a function to strip the persistence identifiers from the
    state. Otherwise, they persist and are mutated each time through
    the event-state-handler, resulting in modified stats for each event.
    
    Resolves #16

--- src/clj/foosball_score/server.clj
+++ src/clj/foosball_score/server.clj
@@ -19,6 +19,16 @@
   (state/update-state! (patch @state/state event))
   (push-event! event))
 
+(defn- persist-using!
+  "Handles persistence of winners or losers using the provided persistence
+  method. Returns the state with the winners / losers stripped out of the state"
+  [state k fpersist]
+  (if-let [wls (k state)]
+    (do
+      (doseq [wl wls] (fpersist wl))
+      (dissoc state k))
+    state))
+
 (defn event-state-handler
   [event]
   (let [state @state/state
@@ -26,16 +36,11 @@
                            (state/event->state event)
                            (statistics/win-loss-stats))]
     (if (nil? next-state) state
-      (do
+      (let [next-state (-> next-state
+                           (persist-using! :winners persist/win-for!)
+                           (persist-using! :losers persist/loss-for!))]
         (push-event! (delta state next-state))
-        (state/update-state! next-state)))
-    (if-let [winners (:winners next-state)]
-      (doseq [winner winners]
-        (persist/win-for! winner)))
-    (if-let [losers (:losers next-state)]
-      (doseq [loser losers]
-        (persist/loss-for! loser)))
-    next-state))
+        (state/update-state! next-state)))))
 
 (defn every-second
   "Invoked every second to sync the time across clients"
