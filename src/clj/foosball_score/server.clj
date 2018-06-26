1e2a6b6ed85ce7c4b503d14ad65f92699c342161
Ian McIntyre <ianpmcintyre@gmail.com>
2018-06-25 21:50:30 -0400

    Add docs

--- src/clj/foosball_score/server.clj
+++ src/clj/foosball_score/server.clj
@@ -53,6 +53,9 @@
     state))
 
 (defn- event-state-handler
+  "Pipe the current state and event through a variety of state-transforming functions. If
+  one of them every returns nil, the operation is aborted, and the return is nil. Otherwise,
+  the return is a new state."
   [state event]
   (some-> state
           (state/event->state event)
@@ -62,21 +65,27 @@
           (persist-using! :tiers persist/tie-for!)))
 
 (defn event-state-task!
+  "Spawns a task to process events that change the Foosball state. Events are pushed onto
+  a channel, and the event with a new state is pushed onto an output channel. The return
+  is a tuple of the input and output channels. The output channel pushes messages of
+  [event new-state]."
   []
   (let [in  (chan)
         out (chan)]
-    (go-loop [event   (<! in)
-              state @state/state]
+    (go-loop [event   (<! in)       ; Get event from the input channel, waiting if necessary...
+              state @state/state]   ; ...then immediately grab the current state, which may have changed
       (when-let [next-state (event-state-handler state event)]
-        (if (state/game-over? next-state) (post-slack-msg! (slack/game-outcome next-state)))
+        (if (state/game-over? next-state)
+            (post-slack-msg! (slack/game-outcome next-state)))
         (push-event! (delta state next-state))
         (state/update-state! next-state)
         (put! out [event next-state]))
-      (recur (<! in) @state/state))
+      (recur (<! in)              ; Get the event from the input channel, waiting if necessary...
+             @state/state))       ; ...then immediately grab the current state, which may have changed
     [in out]))
 
 (defn every-second
-  "Invoked every second to sync the time across clients"
+  "Invoked every second to sync the time across clients. Pushes an event on the provided channel."
   [event-chan]
   (put! event-chan :tick))
 
